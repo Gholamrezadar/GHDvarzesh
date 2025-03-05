41a0daed9337d986f9fc83c947024d3132214923
Gholamreza Dar <rezadar1378@gmail.com>
2025-03-05 18:09:56 +0330

    started the players page

---
+++ app/blurdemo/page.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center relative">
+            <p className="overflow-scroll h-96">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam molestias laborum, aperiam corporis sunt laboriosam dolor! Laboriosam incidunt dolorem accusantium animi nobis non sequi harum, mollitia rem quam aut nulla!
+                Fuga explicabo nihil, excepturi quaerat asperiores a! Esse voluptatem molestias error modi dolore eos iste, ratione illum incidunt quia eum cum voluptate maxime fugiat vero reprehenderit suscipit, repudiandae placeat natus?
+                Consequatur voluptatem natus accusantium illum impedit et nobis sint unde, aut numquam tempore qui quidem dolor, asperiores nisi architecto. Commodi, accusamus. Dolore distinctio vitae repellat exercitationem. Culpa consectetur sapiente voluptatibus?
+                Corrupti, eveniet sequi qui necessitatibus excepturi nisi? Sed corrupti laudantium rerum amet neque cumque facilis sequi culpa unde rem corporis eveniet nemo ut doloribus, fugiat commodi aliquam fuga nostrum quasi.
+                Architecto error, ullam ad consectetur fuga non hic ex quaerat voluptates incidunt libero, nesciunt eius. Nulla, aperiam corrupti! Officiis commodi perferendis quae hic cum iusto exercitationem delectus esse accusantium voluptates?
+                Commodi quia veritatis, beatae illo exercitationem est, assumenda doloribus enim maxime officia quos ad accusantium dignissimos animi, blanditiis dolorum! Placeat sint cumque praesentium veritatis officia totam error harum temporibus ut?
+                Blanditiis beatae voluptas omnis error enim est, laudantium mollitia impedit numquam consequuntur aliquid laborum sed fugiat illo quo eaque libero a inventore odit. Molestias quas, doloremque inventore ipsum aspernatur nihil.
+                Distinctio, eligendi! Rem veritatis deserunt porro. Exercitationem error odio assumenda, a nisi dolores nobis sed nulla ratione aut quaerat inventore autem cum sunt saepe. Cum hic perspiciatis in consectetur quasi.
+                Facere quos officiis rem fugiat molestias vero, numquam ipsum magni minima delectus illum id qui quo, cum, exercitationem unde odio? Quos assumenda est iure explicabo veritatis quaerat obcaecati tempore totam.
+                Minima laborum, tempora eveniet reprehenderit esse nostrum at quaerat officiis ullam. Inventore, enim magnam. Quasi perspiciatis praesentium commodi quae, aliquam facilis magni a maiores? Dolor provident fugiat voluptatem voluptates maxime.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi debitis voluptas fugit non repudiandae earum illum fuga repellendus officiis vel nesciunt, rem culpa illo mollitia atque! Voluptatum quae facere voluptate?
+                Alias tempora et fugit. Quis accusantium itaque incidunt voluptatibus architecto accusamus earum deleniti ipsam mollitia maxime nam ducimus doloremque esse, est quas et voluptatem, maiores amet! Illum ratione distinctio dignissimos.
+                Ex, accusamus debitis odio ea, neque iure sed laudantium dolor veniam ullam, ipsum aperiam fuga explicabo. Beatae nobis expedita debitis. Error reprehenderit, quo id illum saepe odit? Beatae, excepturi expedita!
+                Magni soluta veritatis dolore. Consequuntur labore eum ea, molestias ad quis, eaque reprehenderit animi deleniti distinctio, temporibus nesciunt? Eum provident tenetur consequatur excepturi soluta molestias dicta alias est atque accusantium.
+                Deleniti eligendi quis voluptatum aliquid tempore nam architecto, facilis doloremque? Officiis, inventore deleniti at cum cupiditate, fuga suscipit, commodi libero molestiae vero dolorum veniam rem pariatur praesentium? Veritatis, asperiores maxime!
+                Quod dicta error, ducimus iusto velit eius quis esse tempore voluptates aut sit culpa unde voluptatibus commodi possimus eligendi reprehenderit porro cum eveniet quas nostrum. Corrupti perspiciatis mollitia quo inventore?
+                Cum, debitis pariatur. Neque debitis expedita, ab doloremque culpa eaque vel hic nobis consequatur, deserunt doloribus voluptas, laborum vero quo saepe? Ea corporis omnis nostrum dignissimos, pariatur tenetur officiis sit.
+                Consequatur eligendi at expedita commodi omnis cumque atque deserunt ipsum ut dolorum, fugit debitis eaque dolor illo pariatur. Aliquam quaerat, repellendus ratione voluptatibus animi possimus! Similique repudiandae repellat numquam enim.
+                Dolor perspiciatis repellat fugit hic inventore corrupti accusantium quos, minima consectetur id nihil distinctio nisi repudiandae adipisci quaerat labore, quidem, alias ad? Deleniti corporis nobis ea recusandae iure est quia.
+                Nisi vitae at provident asperiores deserunt quod unde odio eaque fugiat recusandae eveniet in modi blanditiis officia rem, quam pariatur. Repudiandae similique cumque veritatis deserunt vero consequatur id eligendi ut.
+                Enim vel cum, doloremque sit mollitia vero repudiandae et pariatur ut commodi sequi labore corrupti. Beatae odit nesciunt delectus similique neque, nemo culpa voluptatibus praesentium libero, temporibus perspiciatis optio quae?
+                Alias corrupti corporis esse aperiam laboriosam nobis sit, qui minus tempore optio ab! Tenetur distinctio rerum deserunt neque unde modi nemo a dignissimos officia, quod assumenda iste, iure qui culpa.
+                Vitae, similique aliquid! Asperiores nam mollitia vitae unde magnam corrupti possimus nobis qui hic consectetur ipsum distinctio, perspiciatis id, eum aspernatur. Incidunt, voluptatibus quod consequatur corrupti enim fugiat repellendus. Rerum.
+                Eius quam impedit quas repudiandae quo reprehenderit voluptatum? Rerum beatae sapiente accusamus cum nesciunt esse inventore vel saepe eligendi magnam sequi tenetur nihil porro omnis consequatur id atque, sed at?
+                Alias laudantium, autem dicta exercitationem rerum accusamus, officia ipsum atque quis reprehenderit harum, consequatur quibusdam quas. Fugit, maiores impedit, quae laborum porro, voluptate expedita possimus aperiam non autem beatae natus.
+                Quo alias assumenda, maiores veniam amet debitis rerum culpa adipisci totam sunt, earum expedita ullam repellendus in corporis inventore harum aspernatur nemo recusandae aut nisi omnis. Veniam explicabo asperiores voluptatem.
+                Accusamus, reprehenderit! Quae doloremque dolore cupiditate odit dolorum optio! Blanditiis explicabo praesentium voluptatum in, laudantium natus nihil? Odit obcaecati facere distinctio maxime repudiandae quia aperiam alias repellendus veritatis eaque. Est.
+                Esse amet repellat quia magnam dolore. Vero doloremque voluptate autem perferendis sapiente culpa nisi commodi sunt nobis distinctio! Nesciunt neque officiis inventore veritatis reiciendis? Aliquam minima unde omnis doloremque eum.
+                Quam ea asperiores totam reiciendis dolorum ipsum, sint est eaque consectetur voluptatum blanditiis, tempore ratione. Ad similique nemo voluptatibus facilis quisquam aspernatur ipsa, harum tenetur officiis ab dolorum earum quo?
+                Eum ipsam culpa odio excepturi natus ex obcaecati vitae quia magnam ducimus praesentium aperiam iusto numquam mollitia enim nemo, inventore sit velit cum? Commodi architecto blanditiis officia repudiandae libero harum!
+            </p>
+
+            <div className="progressive-blur-container">
+                <div className="blur-filter"></div>
+                <div className="blur-filter"></div>
+                <div className="blur-filter"></div>
+                <div className="blur-filter"></div>
+                <div className="blur-filter"></div>
+                <div className="blur-filter"></div>
+                <div className="blur-filter"></div>
+                <div className="gradient"></div>
+            </div>
+
+
+        </div>
+    );
+}
